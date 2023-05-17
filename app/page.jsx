b2496364e17d85cd19a47041b025f09d5175b744
Adam Bagrianow <bagrianow@gmail.com>
2023-05-17 11:33:38 +0200

    making Feed compontent -

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,21 @@
+import Feed from "@components/Feed";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Promts</span>
+      </h1>
+      <p className="desc text-center">
+        Promtiopia is an open-source AI promtping tool for modern wordl to
+        sicover, ceate and share creative prompts
+      </p>
+
+      <Feed />
+    </section>
+  );
 };
 
 export default Home;
