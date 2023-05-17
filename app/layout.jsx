ff3441c0bb24955c03ee84b09f21b1fd9bc8cd88
Adam Bagrianow <bagrianow@gmail.com>
2023-05-17 11:26:20 +0200

    ready to start developing project

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,19 +1,18 @@
 import "@styles/globals.css";
-import { Children } from "react";
 
 export const metadata = {
   title: "Promptiompia",
   description: "Discover & share Ai prompts",
 };
 
-const RootLayout = ({ Children }) => {
+const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
         <div className="main">
-          <div className="gradient"></div>
+          <div className="gradient" />
         </div>
-        <main className="app">{Children}</main>
+        <main className="app">{children}</main>
       </body>
     </html>
   );
